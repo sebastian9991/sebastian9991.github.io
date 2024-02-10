6e492a5110704863b87c9257ea5d834267781f6d
Sebastian C. Sabry <sebastiancsabry@system76-pc.localdomain>
2024-02-09 18:06:29 -0800

    contentLayer was missing

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,12 @@
+import { withContentlayer } from "next-contentlayer";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
   output: "export",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
   },
 };
 
-export default nextConfig;
+export default withContentlayer(nextConfig);
